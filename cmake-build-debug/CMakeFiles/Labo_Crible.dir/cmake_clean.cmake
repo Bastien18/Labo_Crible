5b646ebfcc63c7a4a91b2ad81c82fb331a6bbaec
Bastien <bastien.pil01@gmail.com>
2021-11-18 16:39:24 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Labo_Crible.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labo_Crible.dir/main.cpp.obj"
+  "Labo_Crible.exe"
+  "Labo_Crible.exe.manifest"
+  "Labo_Crible.pdb"
+  "libLabo_Crible.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labo_Crible.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Labo_Crible.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Labo_Crible.
+# This may be replaced when dependencies are built.
