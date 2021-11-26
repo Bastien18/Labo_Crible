926518225192fca8d3ab490adafb0e7847bd7a43
emilie-h <emilie.bressoud@heig-vd.ch>
2021-11-26 17:36:35 +0100

    fichiers crible(test)

--- complement.cpp
+++ complement.cpp
@@ -5,26 +5,10 @@
 #include "complement.h"
 
 using namespace std;
-//
-//const bool O = true;
-//const bool X = false;
-//const bool CRIBLE_INITIAL[] = {X, O, O, X, O, X, O, X, X, X,
-//                               O, X, O, X, X, X, O, X, O, X,
-//                               X, X, O, X, X, X, X, X, O, X,
-//                               O, X, X, X, X, X, O, X, X, X,
-//                               O, X, O, X, X, X, O, X, X, X,
-//                               X, X, O, X, X, X, X, X, O, X,
-//                               O, X, X, X, X, X, O, X, X, X,
-//                               O, X, O, X, X, X, X, X, O, X,
-//                               X, X, O, X, X, X, X, X, O, X,
-//                               X, X, X, X, X, X, O, X, X, X};
-//const size_t TAILLE_INITIAL = sizeof(CRIBLE_INITIAL) / sizeof(bool);
-//
-//
-// tableau de nbre 1er
+
 
 size_t nbre1er(const bool tab[], const size_t tailleInitial, unsigned tab1er[], const size_t capacite) {
-   unsigned posNbr1er = 0;
+   size_t posNbr1er = 0;
 
    //parcourt le tableau de booléens et remplit le tableau des nombres premiers
    for (unsigned pos = 0; pos < tailleInitial; ++pos) {
@@ -32,7 +16,7 @@ size_t nbre1er(const bool tab[], const size_t tailleInitial, unsigned tab1er[], 
          tab1er[posNbr1er] = pos + 1;
          ++posNbr1er;
          if (posNbr1er == capacite) {
-            return posNbr1er;
+            break;
          }
       }
    }
